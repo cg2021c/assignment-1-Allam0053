98e63fb320efcd863f5ba8e6f1943445105ed64f
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2021-12-01 22:19:49 +0700

    iam done with it

--- tugas-3/main-tugas-3.js
+++ tugas-3/main-tugas-3.js
@@ -41,12 +41,12 @@ function main() {
 	}
 
 	var indices_left = new Uint16Array(indices_);
-	for (var it = 0; it < indices_left.length; it++) {
-		indices_left[it] = indices_left[it] + ( 24 * 7 ); //start the index on the end of the last index of the right obj
-	}
+	// for (var it = 0; it < indices_left.length; it++) {
+	// 	indices_left[it] = indices_left[it] + ( 24 * 7 ); //start the index on the end of the last index of the right obj
+	// }
 
-	vertices_.push(...vertices_left);
-	indices_.push(...indices_left);
+	// vertices_.push(...vertices_left);
+	// indices_.push(...indices_left);
 
 	// create new box of light
 	var box_center = [0.0, 0.0, 0.0];
@@ -90,7 +90,7 @@ function main() {
 	}
 	
 
-	var cube_box = 24 * 14;
+	var cube_box = 24 * 7;
 	var box_index = [
 		// cube b1
 		0 + cube_box, 1 + cube_box, 2 + cube_box,     0 + cube_box, 2 + cube_box, 3 + cube_box,     // Face A
@@ -104,173 +104,48 @@ function main() {
 	vertices_.push(...box);
 	indices_.push(...box_index);
 
-	// Create a linked-list for storing the vertices data
-	var vertexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices_), gl.STATIC_DRAW);
 
-	// Create a linked-list for storing the indices data
-	var indexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices_), gl.STATIC_DRAW);
-
-	var vertexShaderSource = `
-			attribute vec3 aPosition;
-			attribute vec3 aColor;
-			attribute vec3 aNormal;
-			varying vec3 vPosition;
-			varying vec3 vColor;
-			varying vec3 vNormal;
-			uniform mat4 uModel;
-			uniform mat4 uView;
-			uniform mat4 uProjection;
-			void main() {
-					vec4 originalPosition = vec4(aPosition, 1.);
-					gl_Position = uProjection * uView * uModel * originalPosition;
-					vPosition = (uModel * originalPosition).xyz;
-					vColor = aColor;
-					vNormal = aNormal;
-			}
-	`;
-
-	var fragmentShaderSource = `
-			precision mediump float;
-			varying vec3 vPosition;
-			varying vec3 vColor;
-			varying vec3 vNormal;
-			uniform vec3 uAmbientConstant;   // Represents the light color
-			uniform float uAmbientIntensity;
-			uniform vec3 uDiffuseConstant;  // Represents the light color
-			uniform vec3 uLightPosition;
-			uniform mat3 uNormalModel;
-			uniform vec3 uSpecularConstant; // Represents the light color
-			uniform vec3 uViewerPosition;
-			void main() {
-					
-					// Calculate the ambient component
-					vec3 ambient = uAmbientConstant * uAmbientIntensity;
-					
-					// Prepare the diffuse components
-					vec3 normalizedNormal = normalize(uNormalModel * vNormal);
-					vec3 vLight = uLightPosition - vPosition;
-					vec3 normalizedLight = normalize(vLight);
-					vec3 diffuse = vec3(0., 0., 0.);
-					float cosTheta = max(dot(normalizedNormal, normalizedLight), 0.);
-
-					// Prepare the specular components
-					vec3 vReflector = 2.0 * cosTheta * vNormal - (vLight);
-					// or using the following expression
-					// vec3 vReflector = reflect(-vLight, vNormal);
-					vec3 vViewer = uViewerPosition - vPosition;
-					vec3 normalizedViewer = normalize(vViewer);
-					vec3 normalizedReflector = normalize(vReflector);
-					float shininessConstant = 100.0;
-					vec3 specular = vec3(0., 0., 0.);
-					float cosPhi = max(dot(normalizedViewer, normalizedReflector), 0.);
-					
-					// Calculate the phong reflection effect
-					if (cosTheta > 0.) {
-							diffuse = uDiffuseConstant * cosTheta;
-					}
-					if (cosPhi > 0.) {
-							specular = uSpecularConstant * pow(cosPhi, shininessConstant);
-					}
-					vec3 phong = ambient + diffuse + specular;
-
-					// Apply the shading
-					gl_FragColor = vec4(phong * vColor, 1.);
-					gl_FragColor.w = 0.5; //transparency
-			}
-	`;
-
+	// shader init
 	// Create .c in GPU
 	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 	gl.shaderSource(vertexShader, vertexShaderSource);
-	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-	gl.shaderSource(fragmentShader, fragmentShaderSource);
+	var fragmentShaderL = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragmentShaderL, fragmentShaderSourceL);
+	var fragmentShaderR = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragmentShaderR, fragmentShaderSourceR);
+
 
 	// Compile .c into .o
 	gl.compileShader(vertexShader);
-	gl.compileShader(fragmentShader);
+	gl.compileShader(fragmentShaderL);
+	gl.compileShader(fragmentShaderR);
 
 	// Prepare a .exe shell (shader program)
-	var shaderProgram = gl.createProgram();
+	var shaderProgramL = gl.createProgram();
+	var shaderProgramR = gl.createProgram();
 
 	// Put the two .o files into the shell
-	gl.attachShader(shaderProgram, vertexShader);
-	gl.attachShader(shaderProgram, fragmentShader);
+	gl.attachShader(shaderProgramL, vertexShader);
+	gl.attachShader(shaderProgramL, fragmentShaderL);
+	gl.attachShader(shaderProgramR, vertexShader);
+	gl.attachShader(shaderProgramR, fragmentShaderR);
 
 	// Link the two .o files, so together they can be a runnable program/context.
-	gl.linkProgram(shaderProgram);
+	gl.linkProgram(shaderProgramL);
+	gl.linkProgram(shaderProgramR);
 
-	// Start using the context (analogy: start using the paints and the brushes)
-	gl.useProgram(shaderProgram);
-
-	// Teach the computer how to collect
-	//  the positional values from ARRAY_BUFFER
-	//  to each vertex being processed
-	var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-	gl.vertexAttribPointer(
-			aPosition, 
-			3, 
-			gl.FLOAT, 
-			false, 
-			9 * Float32Array.BYTES_PER_ELEMENT, 
-			0
-	);
-	gl.enableVertexAttribArray(aPosition);
-	var aColor = gl.getAttribLocation(shaderProgram, "aColor");
-	gl.vertexAttribPointer(
-			aColor,
-			3,
-			gl.FLOAT,
-			false, 
-			9 * Float32Array.BYTES_PER_ELEMENT,
-			3 * Float32Array.BYTES_PER_ELEMENT
-	);
-	gl.enableVertexAttribArray(aColor);
-	var aNormal = gl.getAttribLocation(shaderProgram, "aNormal");
-	gl.vertexAttribPointer(
-			aNormal,
-			3,
-			gl.FLOAT,
-			false, 
-			9 * Float32Array.BYTES_PER_ELEMENT,
-			6 * Float32Array.BYTES_PER_ELEMENT
-	);
-	gl.enableVertexAttribArray(aNormal);
-
-	// Lighting and Shading
-	// AMBIENT
-	var uAmbientConstant = gl.getUniformLocation(shaderProgram, "uAmbientConstant");
-	var uAmbientIntensity = gl.getUniformLocation(shaderProgram, "uAmbientIntensity");
-	// gl.uniform3fv(uAmbientConstant, [1.0, 0.5, 0.0]);    // orange light
-	gl.uniform3fv(uAmbientConstant, [1.0, 1.0, 1.0]);       // white light
-	gl.uniform1f(uAmbientIntensity, 0.253); // 20% of light
-	// DIFFUSE
-	var uDiffuseConstant = gl.getUniformLocation(shaderProgram, "uDiffuseConstant");
-	var uLightPosition = gl.getUniformLocation(shaderProgram, "uLightPosition");
-	var uNormalModel = gl.getUniformLocation(shaderProgram, "uNormalModel");
-	gl.uniform3fv(uDiffuseConstant, [1.0, 1.0, 1.0]);   // white light
-	gl.uniform3fv(uLightPosition, box_center);    // light position
-
-	// Perspective projection
-	var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
-	var perspectiveMatrix = glMatrix.mat4.create();
-	glMatrix.mat4.perspective(perspectiveMatrix, Math.PI/3, 1.0, 0.5, 10.0);
-	gl.uniformMatrix4fv(uProjection, false, perspectiveMatrix);
-
-	var freeze = false;
-	// Interactive graphics with mouse
-	function onMouseClick(event) {
-			freeze = !freeze;
-	}
-	document.addEventListener("click", onMouseClick);
+	var cur_program = '';
+	
 	// Interactive graphics with keyboard
 	var cameraX = 0.0;
 	var cameraY = 2.0;
 	var cameraZ = 5.0;
-	var uView = gl.getUniformLocation(shaderProgram, "uView");
+
+	var uLightPosition = gl.getUniformLocation(shaderProgramL, "uLightPosition");
+	var uLightPositionR = gl.getUniformLocation(shaderProgramR, "uLightPosition");
+
+	gl.useProgram(shaderProgramL);
+	var uView = gl.getUniformLocation(shaderProgramL, "uView");
 	var viewMatrix = glMatrix.mat4.create();
 	glMatrix.mat4.lookAt(
 			viewMatrix,
@@ -280,9 +155,28 @@ function main() {
 	);
 	gl.uniformMatrix4fv(uView, false, viewMatrix);
 
+	gl.useProgram(shaderProgramR);
+	var uViewR = gl.getUniformLocation(shaderProgramR, "uView");
+	gl.uniformMatrix4fv(uViewR, false, viewMatrix);
+	var viewMatrixR = glMatrix.mat4.create();
+	glMatrix.mat4.lookAt(
+			viewMatrixR,
+			[cameraX, cameraY, cameraZ],    // the location of the eye or the camera
+			[cameraX, 0.0, -10],        // the point where the camera look at
+			[0.0, 1.0, 0.0]
+	);
+	gl.uniformMatrix4fv(uViewR, false, viewMatrixR);
+
 	
+	var freeze = false;
+	// Interactive graphics with mouse
+	function onMouseClick(event) {
+			freeze = !freeze;
+	}
+	document.addEventListener("click", onMouseClick);
+
   function changeBoxPos(xyz, mov) {
-		var index_start = 9 * 24 * 14;
+		var index_start = 9 * 24 * 7;
 		for (var it = 0; it < box.length; it += 9) {
 			vertices_[index_start + it + xyz] += mov;
 		}
@@ -320,7 +214,8 @@ function main() {
 			box_center[2]+=0.1;
 			changeBoxPos(2, 0.1);
 		}
-		gl.uniform3fv(uLightPosition, new Float32Array(box_center));
+		if(cur_program == 'l') gl.uniform3fv(uLightPosition, box_center);
+		if(cur_program == 'l') gl.uniform3fv(uLightPositionR, box_center);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices_), gl.STATIC_DRAW);
 	}
 
@@ -339,7 +234,8 @@ function main() {
 					[cameraX, 0.0, -10],        // the point where the camera look at
 					[0.0, 1.0, 0.0]
 			);
-			gl.uniformMatrix4fv(uView, false, viewMatrix);
+			if(cur_program == 'l') gl.uniformMatrix4fv(uView, false, viewMatrix);
+			if(cur_program == 'r') gl.uniformMatrix4fv(uViewR, false, viewMatrix);
 	}
 	function onKeyup(event) {
 			if (event.keyCode == 32) freeze = false;
@@ -349,20 +245,93 @@ function main() {
 	document.addEventListener("keydown", onKeydown);
 	document.addEventListener("keyup", onKeyup);
 	
-	// SPECULAR
-	var uSpecularConstant = gl.getUniformLocation(shaderProgram, "uSpecularConstant");
-	var uViewerPosition = gl.getUniformLocation(shaderProgram, "uViewerPosition");
-	gl.uniform3fv(uSpecularConstant, [1.0, 1.0, 1.0]);  // white light
-	gl.uniform3fv(uViewerPosition, [cameraX, cameraY, cameraZ]);
 
 	var speedRaw = 1;
 	var speedX = speedRaw / 600;
 	var speedY = 2 * speedRaw / 600;
 	var changeX = 0;
 	var changeY = 0;
-	var uModel = gl.getUniformLocation(shaderProgram, "uModel");
-	function render() {
-			if (!freeze) {  // If it is not freezing, then animate the rectangle
+
+
+	function renderRight(currShader, currVertices, currIndices, option){
+				// Start using the context (analogy: start using the paints and the brushes)
+				gl.useProgram(currShader);
+				cur_program = 'r';
+						
+				// Create a linked-list for storing the vertices data
+				var vertexBuffer = gl.createBuffer();
+				gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+				gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(currVertices), gl.STATIC_DRAW);
+
+				// Create a linked-list for storing the indices data
+				var indexBuffer = gl.createBuffer();
+				gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+				gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(currIndices), gl.STATIC_DRAW);
+
+
+				// Teach the computer how to collect
+				//  the positional values from ARRAY_BUFFER
+				//  to each vertex being processed
+				var aPosition = gl.getAttribLocation(currShader, "aPosition");
+				gl.vertexAttribPointer(
+						aPosition, 
+						3, 
+						gl.FLOAT, 
+						false, 
+						9 * Float32Array.BYTES_PER_ELEMENT, 
+						0
+				);
+				gl.enableVertexAttribArray(aPosition);
+				var aColor = gl.getAttribLocation(currShader, "aColor");
+				gl.vertexAttribPointer(
+						aColor,
+						3,
+						gl.FLOAT,
+						false, 
+						9 * Float32Array.BYTES_PER_ELEMENT,
+						3 * Float32Array.BYTES_PER_ELEMENT
+				);
+				gl.enableVertexAttribArray(aColor);
+				var aNormal = gl.getAttribLocation(currShader, "aNormal");
+				gl.vertexAttribPointer(
+						aNormal,
+						3,
+						gl.FLOAT,
+						false, 
+						9 * Float32Array.BYTES_PER_ELEMENT,
+						6 * Float32Array.BYTES_PER_ELEMENT
+				);
+				gl.enableVertexAttribArray(aNormal);
+
+				// Lighting and Shading
+				// AMBIENT
+				var uAmbientConstant = gl.getUniformLocation(currShader, "uAmbientConstant");
+				var uAmbientIntensity = gl.getUniformLocation(currShader, "uAmbientIntensity");
+				// gl.uniform3fv(uAmbientConstant, [1.0, 0.5, 0.0]);    // orange light
+				gl.uniform3fv(uAmbientConstant, [1.0, 1.0, 1.0]);       // white light
+				gl.uniform1f(uAmbientIntensity, 0.253); // 20% of light
+				// DIFFUSE
+				var uDiffuseConstant = gl.getUniformLocation(currShader, "uDiffuseConstant");
+				
+				var uNormalModel = gl.getUniformLocation(currShader, "uNormalModel");
+				gl.uniform3fv(uDiffuseConstant, [1.0, 1.0, 1.0]);   // white light
+			  if (option == 'r')	gl.uniform3fv(uLightPositionR, box_center);    // light position
+				if (option == 'l')	gl.uniform3fv(uLightPosition, box_center);
+
+				// Perspective projection
+				var uProjection = gl.getUniformLocation(currShader, "uProjection");
+				var perspectiveMatrix = glMatrix.mat4.create();
+				glMatrix.mat4.perspective(perspectiveMatrix, Math.PI/3, 1.0, 0.5, 10.0);
+				gl.uniformMatrix4fv(uProjection, false, perspectiveMatrix);
+				
+				// SPECULAR
+				var uSpecularConstant = gl.getUniformLocation(currShader, "uSpecularConstant");
+				var uViewerPosition = gl.getUniformLocation(currShader, "uViewerPosition");
+				gl.uniform3fv(uSpecularConstant, [1.0, 1.0, 1.0]);  // white light
+				gl.uniform3fv(uViewerPosition, [cameraX, cameraY, cameraZ]);
+				var uModel = gl.getUniformLocation(currShader, "uModel");
+
+				if (!freeze) {  // If it is not freezing, then animate the rectangle
 					if (changeX >= 0.5 || changeX <= -0.5) speedX = -speedX;
 					// if (changeY >= 0.5 || changeY <= -0.5) speedY = -speedY;
 					changeX = changeX + speedX;
@@ -377,22 +346,37 @@ function main() {
 					var normalModelMatrix = glMatrix.mat3.create();
 					glMatrix.mat3.normalFromMat4(normalModelMatrix, modelMatrix);
 					gl.uniformMatrix3fv(uNormalModel, false, normalModelMatrix);
-			}
-			gl.clearColor(0.0, 0.0, 0.0, 1.0);
-			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-			gl.enable(gl.DEPTH_TEST);
-			//transparency func
-			gl.depthFunc(gl.LEQUAL)
-			gl.depthMask(false);
-			gl.enable(gl.BLEND);
-			gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+				}
+				gl.enable(gl.DEPTH_TEST);
+				//transparency func
+				if (option == 'r') {
+					gl.depthFunc(gl.LESS);
+					gl.depthMask(true);
+					gl.disable(gl.BLEND);
+					gl.blendFunc(gl.ONE, gl.ONE);
+				} else {
+					gl.enable(gl.DEPTH_TEST);
+					gl.depthFunc(gl.LEQUAL)
+					gl.depthMask(false);
+					gl.enable(gl.BLEND);
+					gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+				}
 
-			var primitive = gl.TRIANGLES;
-			var offset = 0;
-			var nVertex = indices_.length;
-			// gl.drawArrays(primitive, offset, nVertex);
-			gl.drawElements(primitive, nVertex, gl.UNSIGNED_SHORT, offset);
-			requestAnimationFrame(render);
+				var primitive = gl.TRIANGLES;
+				var offset = 0;
+				var nVertex = currIndices.length;
+				// gl.drawArrays(primitive, offset, nVertex);
+				gl.drawElements(primitive, nVertex, gl.UNSIGNED_SHORT, offset);
+				cur_program = 'r';
+	}
+
+	function render() {
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		// renderLeft();
+		renderRight(shaderProgramL, vertices_left, indices_left, 'l');
+		renderRight(shaderProgramR, vertices_, indices_, 'r');
+		requestAnimationFrame(render);
 	}
 	requestAnimationFrame(render);
 }
